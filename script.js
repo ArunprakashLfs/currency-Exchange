5cad674718676e98aab43787e3784ed027e85f9f
ArunprakashLfs <arunprakash7871@gmail.com>
2023-03-26 21:22:34 +0530

    add event listners

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ let inputCurEl = document.getElementById("input-currency");
 let outputvalEl = document.getElementById("output-value");
 let outputCurEl = document.getElementById("output-currency");
 let formEl = document.getElementById("form");
+
+
+//add event listners
+formEl.addEventListener("submit", function(e){
+    e.preventDefault();
+    let input = inputvalEl.value;
+    let inputcur = inputCurEl.value;
+    console.log(input, inputcur);
+})
