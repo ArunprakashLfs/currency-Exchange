4ced6fc06b3bb69fac8819105e783eb5249887f6
ArunprakashLfs <arunprakash7871@gmail.com>
2023-03-26 21:14:01 +0530

    getting input element from html

---
+++ script.js
@@ -0,0 +1,5 @@
+//getting input
+let inputvalEl = document.getElementById("input-value");
+let inputCurEl = document.getElementById("input-currency");
+let outputvalEl = document.getElementById("output-value");
+let outputCurEl = document.getElementById("output-currency")
